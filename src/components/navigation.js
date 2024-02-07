6030b1a80716ccfe20cd7e1c902ba14288505a45
evanjliu <liuevan@oregonstate.edu>
2024-02-07 07:55:22 -0800

    Update

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
   return (
     <nav class="global">
         {/* Add links to Home, Topics, Gallery, Contact, and Staff Pages  */}
-        <Link to="/">Home</Link>
+        <Link to="/home">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
     </nav>
